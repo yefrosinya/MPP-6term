8cf018c682324fc3eeb8a4b16f44bb44b1381b09
yefrosinya <efrosinasologub@gmail.com>
2026-02-23 15:55:31 +0300

    Changed to concurrent dictionary

--- 2lab/Faker/Faker/FakerConf.cs
+++ 2lab/Faker/Faker/FakerConf.cs
@@ -1,9 +1,10 @@
 namespace Faker;
 using System.Linq.Expressions;
+using System.Collections.Concurrent;
 
 public class FakerConf
 {
-    private readonly Dictionary<Type, Dictionary<string, Type>> _customGenerators = new();
+    private readonly ConcurrentDictionary<Type, ConcurrentDictionary<string, Type>> _customGenerators = new();
 
     public void Add<TClass, TProp, TGen>(Expression<Func<TClass, TProp>> selector) 
         where TGen : IValueGenerator
@@ -14,12 +15,10 @@ public class FakerConf
         var type = typeof(TClass);
         var memberName = member.Member.Name;
 
-        if (!_customGenerators.ContainsKey(type))
-            _customGenerators[type] = new Dictionary<string, Type>(StringComparer.OrdinalIgnoreCase);
-
-        _customGenerators[type][memberName] = typeof(TGen);
+        var members = _customGenerators.GetOrAdd(type, _ => new ConcurrentDictionary<string, Type>(StringComparer.OrdinalIgnoreCase));
+        members[memberName] = typeof(TGen);
     }
-
+    
     public Type GetGeneratorType(Type classType, string memberName)
     {
         if (_customGenerators.TryGetValue(classType, out var members) && 
